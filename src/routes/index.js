f16584e84fcdc6922ba115cec1116e653c75479d
LuizGabrielSS <luizgabriel11022001@gmail.com>
2024-09-23 19:32:07 -0300

    feat: adicionando contact screen e plans screen to route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,18 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+//screens
 import NotFoundScreen from '../pages/notfound'
 import HomeScreen from '../pages/home'
 import LoginScreen from '../pages/login'
+import PlansScreen from '../pages/plans'
+import ContactScreen from '../pages/contact'
 
 export default function RoutesFunction(){
 
     return(
         <BrowserRouter
-        // basename='/portfolio'
+        basename='/barberpass'
         >
             <Routes>
                 <Route
@@ -29,6 +32,18 @@ export default function RoutesFunction(){
                     path="/login"
                     element={
                         <LoginScreen/>
+                    }
+                />
+                <Route
+                    path="/plans"
+                    element={
+                        <PlansScreen/>
+                    }
+                />
+                <Route
+                    path="/plans"
+                    element={
+                        <ContactScreen/>
                     }
                 />
             </Routes>
